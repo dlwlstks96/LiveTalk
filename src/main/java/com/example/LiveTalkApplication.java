b481fb1c0fa30703f13d0ea44e300bf0c8953dff
dlwlstks96 <72801465+dlwlstks96@users.noreply.github.com>
2022-07-05 17:38:16 +0900

    Init project

---
+++ src/main/java/com/example/LiveTalkApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveTalkApplication.class, args);
+	}
+
+}
